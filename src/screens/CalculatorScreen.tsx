7c13f68671fa2a57c0023a7312f80dac3896aa68
Pablo Misael Peloc <pmpeloc@gmail.com>
2022-11-24 19:01:49 -0300

    feat: Initial Desing

---
+++ src/screens/CalculatorScreen.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+
+import { styles } from '../theme/base.theme';
+
+export const CalculatorScreen = () => {
+  return (
+    <View>
+      <Text style={styles.text}>Calculator Screen</Text>
+    </View>
+  );
+};
