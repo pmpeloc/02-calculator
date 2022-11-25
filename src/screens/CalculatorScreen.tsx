7dc56c1456d6fdd3e32a45fc58b421af9584b178
Pablo Misael Peloc <pmpeloc@gmail.com>
2022-11-24 22:23:08 -0300

    feat: Add math operators

--- src/screens/CalculatorScreen.tsx
+++ src/screens/CalculatorScreen.tsx
@@ -1,16 +1,26 @@
 /* eslint-disable curly */
-import React, { useState } from 'react';
+import React, { useRef, useState } from 'react';
 import { Text, View } from 'react-native';
 import { CalcButton } from '../components/CalcButton';
 
 import { styles } from '../theme/base.theme';
 
+enum Operators {
+  add,
+  substract,
+  multiply,
+  divide,
+}
+
 export const CalculatorScreen = () => {
   const [number, setNumber] = useState('0');
   const [numberBefore, setNumberBefore] = useState('0');
 
+  const lastOperation = useRef<Operators>();
+
   const clean = () => {
     setNumber('0');
+    setNumberBefore('0');
   };
 
   const makeNumber = (numberText: string) => {
@@ -44,9 +54,39 @@ export const CalculatorScreen = () => {
     }
   };
 
+  const btnDelete = () => {
+    let negative = '';
+    let numberTemp = number;
+    if (number.includes('-')) {
+      negative = '-';
+      numberTemp = number.substring(1);
+    }
+    if (numberTemp.length > 1) {
+      setNumber(negative + numberTemp.slice(0, -1));
+    } else {
+      setNumber('0');
+    }
+  };
+
+  const changeNumberForBefore = () => {
+    if (numberBefore.endsWith('.')) {
+      setNumberBefore(number.slice(0, -1));
+    } else {
+      setNumberBefore(number);
+    }
+    setNumber('0');
+  };
+
+  const btnMathOperation = (operator: Operators) => {
+    changeNumberForBefore();
+    lastOperation.current = operator;
+  };
+
   return (
     <View style={styles.calculatorContainer}>
-      <Text style={styles.resultSmall}>{numberBefore}</Text>
+      {numberBefore !== '0' && (
+        <Text style={styles.resultSmall}>{numberBefore}</Text>
+      )}
       <Text style={styles.result} numberOfLines={1} adjustsFontSizeToFit>
         {number}
       </Text>
@@ -54,29 +94,45 @@ export const CalculatorScreen = () => {
         {/* Button */}
         <CalcButton text="C" color="#9b9b9b" action={clean} />
         <CalcButton text="+/-" color="#9b9b9b" action={positiveNegative} />
-        <CalcButton text="%" color="#9b9b9b" action={clean} />
-        <CalcButton text="/" color="#ff9427" action={clean} />
+        <CalcButton text="del" color="#9b9b9b" action={btnDelete} />
+        <CalcButton
+          text="/"
+          color="#ff9427"
+          action={() => btnMathOperation(Operators.divide)}
+        />
       </View>
       <View style={styles.row}>
         {/* Button */}
         <CalcButton text="7" action={makeNumber} />
         <CalcButton text="8" action={makeNumber} />
         <CalcButton text="9" action={makeNumber} />
-        <CalcButton text="X" color="#ff9427" action={clean} />
+        <CalcButton
+          text="X"
+          color="#ff9427"
+          action={() => btnMathOperation(Operators.multiply)}
+        />
       </View>
       <View style={styles.row}>
         {/* Button */}
-        <CalcButton text="5" action={makeNumber} />
+        <CalcButton text="4" action={makeNumber} />
         <CalcButton text="5" action={makeNumber} />
         <CalcButton text="6" action={makeNumber} />
-        <CalcButton text="-" color="#ff9427" action={clean} />
+        <CalcButton
+          text="-"
+          color="#ff9427"
+          action={() => btnMathOperation(Operators.substract)}
+        />
       </View>
       <View style={styles.row}>
         {/* Button */}
         <CalcButton text="1" action={makeNumber} />
         <CalcButton text="2" action={makeNumber} />
         <CalcButton text="3" action={makeNumber} />
-        <CalcButton text="+" color="#ff9427" action={clean} />
+        <CalcButton
+          text="+"
+          color="#ff9427"
+          action={() => btnMathOperation(Operators.add)}
+        />
       </View>
       <View style={styles.row}>
         {/* Button */}
